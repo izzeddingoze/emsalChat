54c6ae11c685edfd72e4755e6d4a8456193ab656
izzeddingoze <izzeddingoze@gmail.com>
2024-02-23 18:45:02 +0300

    pusher kuruldu, eventlar eklendi - 2

---
+++ app/Events/NewMessage.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use App\Http\Resources\MessageResource;
+use App\Models\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NewMessage
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public Message $message;
+
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+
+    }
+
+
+    public function broadcastOn(): Channel
+    {
+        return new Channel('emsalChat');
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'newMessage';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'message' => new MessageResource($this->message),
+        ];
+    }
+}
---
+++ app/Events/TimeReminder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use App\Http\Resources\MessageResource;
+use App\Models\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TimeReminder
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+
+
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct()
+    {
+
+
+    }
+
+
+    public function broadcastOn(): Channel
+    {
+        return new Channel('emsalChat');
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'timeReminder';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'message' => new MessageResource($this->message),
+        ];
+    }
+}
